c32505cb5923f6d803bce4c75771bc98c43fa477
Vin <hegewaldvin@gmail.com>
2023-04-02 20:17:24 +0200

    update: removing versionKey from db entry in skills

--- src/database/skill.schema.ts
+++ src/database/skill.schema.ts
@@ -1,22 +1,27 @@
 import mongoose from 'mongoose';
 
-const skillSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const skillSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    use: {
+      type: String,
+      required: true,
+    },
+    dotColor: {
+      type: String,
+      required: true,
+    },
   },
-  type: {
-    type: String,
-    required: true,
+  {
+    versionKey: false,
   },
-  use: {
-    type: String,
-    required: true,
-  },
-  dotColor: {
-    type: String,
-    required: true,
-  },
-});
+);
 
 export default mongoose.model('skill', skillSchema);
